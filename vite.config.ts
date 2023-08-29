0e5d8f5969c45c32afc8dfa0a15d3ffc0f870292
Stilllee <orodae@gmail.com>
2023-08-29 17:27:37 +0900

    🔨 : "alias 절대경로 수정" 수정사항 즉시반영 안되는 문제 해결

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ export default defineConfig({
   plugins: [react()], // React 플러그인 사용
   resolve: {
     alias: [
-      { find: '@', replacement: 'src' }, // src 디렉토리에 대한 절대경로 별칭(@) 설정
-      { find: '@components', replacement: 'src/components' }, // components 디렉토리에 대한 절대경로 별칭(@components) 설정
-      { find: '@pages', replacement: 'src/pages' }, // pages 디렉토리에 대한 절대경로 별칭(@pages) 설정
+      { find: '@', replacement: '/src' }, // src 디렉토리에 대한 절대경로 별칭(@) 설정
+      { find: '@components', replacement: '/src/components' }, // components 디렉토리에 대한 절대경로 별칭(@components) 설정
+      { find: '@pages', replacement: '/src/pages' }, // pages 디렉토리에 대한 절대경로 별칭(@pages) 설정
     ],
   },
   css: {
@@ -17,7 +17,4 @@ export default defineConfig({
       },
     },
   },
-  /* build: {
-    chunkSizeWarningLimit: 1600, // 성능을 고려한 청크 크기에 대한 경고 한도 설정 (1600kb)
-  }, */
 });
