76c94e0dca9e2b7b154a21a8e50e42b693ba214d
Stilllee <orodae@gmail.com>
2023-08-29 15:41:09 +0900

    💄 : "전역 스타일 환경설정"

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,13 @@ export default defineConfig({
       { find: '@pages', replacement: 'src/pages' }, // pages 디렉토리에 대한 절대경로 별칭(@pages) 설정
     ],
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '@/assets/styles/global.scss';`,
+      },
+    },
+  },
   /* build: {
     chunkSizeWarningLimit: 1600, // 성능을 고려한 청크 크기에 대한 경고 한도 설정 (1600kb)
   }, */
