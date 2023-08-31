e39ade7013c22cea3148b2ea1154e7a38a616884
Stilllee <orodae@gmail.com>
2023-08-31 23:33:44 +0900

    ✨ : "타입 정의 및 추가기능 구현"
    input의 타입 정의
    password 토글기능 추가

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -4,20 +4,30 @@ import styles from './Input.module.scss';
 interface IInputProps {
   placeholderText: string;
   hintMessage?: string;
+  type: 'text' | 'password' | 'email';
 }
 
-const Input: React.FC<IInputProps> = ({ placeholderText, hintMessage }) => {
+const Input: React.FC<IInputProps> = ({
+  placeholderText,
+  hintMessage,
+  type,
+}) => {
   const [inputValue, setInputValue] = useState('');
+  const [showPW, setShowPW] = useState(false);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
+  };
+
+  const toggleShowPW = () => {
+    setShowPW(!showPW);
   };
 
   return (
     <>
       <div className={styles.inputWrapper}>
         <input
-          type="text"
+          type={type === 'password' && !showPW ? 'password' : 'text'}
           placeholder={placeholderText}
           value={inputValue}
           onChange={handleInputChange}
@@ -26,6 +36,16 @@ const Input: React.FC<IInputProps> = ({ placeholderText, hintMessage }) => {
           <div
             className={styles.clearButton}
             onClick={() => setInputValue('')}
+          ></div>
+        )}
+        {type === 'password' && (
+          <div
+            className={
+              showPW
+                ? `${styles.toogleButton} ${styles.show}`
+                : `${styles.toogleButton}`
+            }
+            onClick={toggleShowPW}
           ></div>
         )}
       </div>
