ad8ccc2e2a14e6dbf2871b6a4f1c9a89c439d128
chaewon <juh7814@gmail.com>
2023-09-05 16:01:32 +0900

    ⚡️ : "스크롤 이벤트 적용" #7

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from '@components/header/Header';
 import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
