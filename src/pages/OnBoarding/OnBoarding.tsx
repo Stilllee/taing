7b8399c84f09ec92c1102459690dd28fb12f25df
chaewon <juh7814@gmail.com>
2023-09-07 11:52:52 +0900

    💄 : "scroll section-2,3 UI 1차 완성" #11

--- src/pages/OnBoarding/OnBoarding.tsx
+++ src/pages/OnBoarding/OnBoarding.tsx
@@ -1,3 +1,4 @@
+import ScrollTitle from '@components/scrollTitle/ScrollTitle';
 import styles from './OnBoarding.module.scss';
 import TaingButton from '@components/taingButton/TaingButton';
 
@@ -14,8 +15,22 @@ const OnBoarding = () => {
         </p>
         <TaingButton />
       </div>
-      <div className={styles.scroll_section_2}></div>
-      <div className={styles.scroll_section_3}></div>
+      <div className={styles.scroll_section_2}>
+        <ScrollTitle
+          title="티빙에만 있는 재미"
+          subtitle="오리지널 콘텐츠를 만나보세요!"
+          paragraph="차별화된 웰메이드 오리지널 콘텐츠"
+        />
+        <div className={styles.swipe_1}></div>
+      </div>
+      <div className={styles.scroll_section_3}>
+        <ScrollTitle
+          title="내가 찾던 재미"
+          subtitle="보고 싶은 콘텐츠를 발견하세요!"
+          paragraph="최신, 인기 TV프로그램, 영화, 해외시리즈, 파라마운트+ 오리지널 및 독점"
+        />
+        <div className={styles.swipe_2}></div>
+      </div>
       <div className={styles.message}>
         <div className={styles.message_logo} />
         <p className={styles.message_title}>지금 시작해보세요</p>
