3627feb6d17ca973970519c617d2d1138c27eda9
Stilllee <orodae@gmail.com>
2023-09-08 15:36:56 +0900

    ✅ : "input 코드수정에 따른 테스트 완료"

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,9 @@
 import styles from './Home.module.scss';
-import Input from '../../components/common/Input/Input';
 
 const Home = () => {
   return (
     <div>
       <h1 className={styles.title}>홈이여유</h1>
-      <Input placeholderText="password" type="password" />
     </div>
   );
 };
