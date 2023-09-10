67ef85d5f9fba7a2035d37816f3b2c827b738513
Stilllee <orodae@gmail.com>
2023-09-10 21:13:57 +0900

    ✨ : "로그인목록 페이지 UI 구현" #14

--- src/pages/LoginSelection/LoginSelection.tsx
+++ src/pages/LoginSelection/LoginSelection.tsx
@@ -1,4 +1,31 @@
+import styles from './LoginSelection.module.scss';
+import { Link, useNavigate } from 'react-router-dom';
+
 const LoginSelection = () => {
-  return <div>로그인목록</div>;
+  const navigate = useNavigate();
+  return (
+    <main className={styles.LoginSelection}>
+      <div className={styles.title}>
+        <p>반가워요!</p>
+        <p>계정을 선택해주세요.</p>
+      </div>
+      <div className={styles.buttonBox}>
+        <button
+          className={styles.startTaing}
+          onClick={() => navigate('/login')}
+          type={'button'}
+        >
+          TAING ID로 시작하기
+        </button>
+        <button className={styles.startGoogle} type={'button'}>
+          구글로 시작하기
+        </button>
+      </div>
+      <div className={styles.findIdBox}>
+        <span className={styles.findIdInfo}>아이디를 잊으셨나요?</span>
+        <Link to={'/findId'}>아이디 찾기</Link>
+      </div>
+    </main>
+  );
 };
 export default LoginSelection;
