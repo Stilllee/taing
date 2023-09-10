76f4c3261f71ad5be585a54e31a73664b89d9cbc
Stilllee <orodae@gmail.com>
2023-09-10 16:56:47 +0900

    ♻️ : "Checkbox 분리에 의한 코드 리팩토링"

--- src/pages/LogIn/LogIn.tsx
+++ src/pages/LogIn/LogIn.tsx
@@ -1,39 +1,17 @@
+import Checkbox from '@components/common/Checkbox/Checkbox';
 import styles from './LogIn.module.scss';
 import Button from '@components/common/Button/Button';
 import Input from '@components/common/Input/Input';
-import { ReactNode, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const LogIn = () => {
-  const [autoLogin, setAutoLogin] = useState(false);
-
-  const toggleAutoLogin = () => {
-    setAutoLogin(prevState => !prevState);
-  };
-
-  const getAutoLoginButton = (): ReactNode => (
-    <button
-      className={styles.autoLoginWrapper}
-      type="button"
-      onClick={toggleAutoLogin}
-      aria-label="Toggle Auto Login"
-    >
-      <div
-        className={`${styles.autoLogin} ${
-          autoLogin && styles.autoLoginChecked
-        }`}
-      />
-      <span className={`${autoLogin && styles.labelChecked}`}>자동로그인</span>
-    </button>
-  );
-
   return (
     <main className={styles.LogIn}>
       <h1 className={styles.title}>TAING ID 로그인</h1>
       <form className={styles.loginForm}>
         <Input type={'text'} placeholderText={'아이디'} />
         <Input type={'password'} placeholderText={'비밀번호'} />
-        {getAutoLoginButton()}
+        <Checkbox id={'auto'} label={'자동로그인'} />
         <Button type={'submit'} title={'로그인하기'} state={'login'} />
       </form>
       <div className={styles.pageLink}>
