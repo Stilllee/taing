b5b337fdefe32208c9110a1d7ed42ff6b9bdf11c
chaewon <juh7814@gmail.com>
2023-09-10 15:08:58 +0900

    🚚 : "loginSelection 으로 path 수정"

--- src/components/taingButton/TaingButton.tsx
+++ src/components/taingButton/TaingButton.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import styles from './TaingButton.module.scss';
 const TaingButton = () => {
   return (
-    <Link to={'/'} className={`${styles.button} intro`}>
+    <Link to={'/loginSelection'} className={`${styles.button} intro`}>
       <div className={styles.icon}></div>
       <p className={styles.title}>새로워진 타잉을 만나보세요!</p>
     </Link>
