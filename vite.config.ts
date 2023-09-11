47c7ec520d08f577aa6ad6737e72f3c37652166d
Stilllee <orodae@gmail.com>
2023-09-11 19:24:25 +0900

    🔧 : "Vite 설정 및 이미지 최적화 플러그인 추가"

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { compression } from 'vite-plugin-compression2';
+import viteImagemin from '@vheemstra/vite-plugin-imagemin';
+import imageminGifSicle from 'imagemin-gifsicle';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngQuant from 'imagemin-pngquant';
+import imageminSvgo from 'imagemin-svgo';
+import imageminWebp from 'imagemin-webp';
 
 export default defineConfig({
-  plugins: [react()], // React 플러그인 사용
+  plugins: [
+    react(), // React 플러그인을 추가하여 JSX와 같은 React 기능을 지원
+    compression(), // 출력 파일을 압축하기 위한 플러그인
+    viteImagemin({
+      // 이미지를 최적화하는 플러그인
+      plugins: {
+        jpg: imageminMozjpeg(),
+        png: imageminPngQuant(),
+        gif: imageminGifSicle(),
+        svg: imageminSvgo(),
+      },
+      makeWebp: {
+        // WebP 형식으로 이미지를 변환
+        plugins: {
+          jpg: imageminWebp(),
+          png: imageminWebp(),
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: [
       { find: '@', replacement: '/src' }, // src 디렉토리에 대한 절대경로 별칭(@) 설정
