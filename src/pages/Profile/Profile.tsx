3644f1dd400ffca9ca58a5fbb4563f8432848ba0
chaewon <juh7814@gmail.com>
2023-09-16 19:14:40 +0900

    ⚡️ : "프로필 페이지 UI완성" #38

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,11 +2,37 @@ import Button from '@components/common/Button/Button';
 import styles from './Profile.module.scss';
 import ProfileTitle from '@components/ProfileTitle/ProfileTitle';
 import { useNavigate } from 'react-router';
+import { useState } from 'react';
 const Profile = () => {
   const navigate = useNavigate();
+  const [activeButtonIndex, setActiveButtonIndex] = useState(-1);
   const onMoveEdit = () => {
     navigate('/profile-edit');
   };
+
+  const profileList = [
+    {
+      profileClassName: `${styles.profilePhoto} ${styles.photoFirst}`,
+      name: '닉네임 1',
+    },
+    {
+      profileClassName: `${styles.profilePhoto} ${styles.photoSecond}`,
+      name: '닉네임 2',
+    },
+    {
+      profileClassName: `${styles.profilePhoto} ${styles.photoThird}`,
+      name: '닉네임 3',
+    },
+    {
+      profileClassName: `${styles.profilePhoto} ${styles.photoFourth}`,
+      name: '닉네임 4',
+    },
+  ];
+
+  const handleButtonClick = (index: number) => {
+    setActiveButtonIndex(index);
+  };
+
   return (
     <div className={styles.profile}>
       <ProfileTitle
@@ -15,22 +41,17 @@ const Profile = () => {
       />
       <div>
         <div className={styles.profileContainer}>
-          <div>
-            <div className={`${styles.profilePhoto} ${styles.photoFirst}`} />
-            <span className={styles.profileName}>닉네임</span>
-          </div>
-          <div>
-            <div className={`${styles.profilePhoto} ${styles.photoSecond}`} />
-            <span className={styles.profileName}>닉네임</span>
-          </div>
-          <div>
-            <div className={`${styles.profilePhoto} ${styles.photoThird}`} />
-            <span className={styles.profileName}>닉네임</span>
-          </div>
-          <div>
-            <div className={`${styles.profilePhoto} ${styles.photoFourth}`} />
-            <span className={styles.profileName}>닉네임</span>
-          </div>
+          {profileList.map((profile, index) => (
+            <div key={index}>
+              <button
+                onClick={() => handleButtonClick(index)}
+                className={`${profile.profileClassName} ${
+                  index === activeButtonIndex ? styles.active : ' '
+                }`}
+              />
+              <p className={styles.profileName}>{profile.name}</p>
+            </div>
+          ))}
         </div>
       </div>
       <Button
