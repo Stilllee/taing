25f051b49769244f4c825c619142741f8e5e6c85
Stilllee <orodae@gmail.com>
2023-09-18 00:15:53 +0900

    ✨ : "Swiper 네비게이션 및 페이징 기능 추가" #45

--- src/components/common/SwiperContent/SwiperContent.tsx
+++ src/components/common/SwiperContent/SwiperContent.tsx
@@ -1,9 +1,13 @@
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
 import styles from './SwiperContent.module.scss';
-import 'swiper/swiper-bundle.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
 import { IImageData } from '@/type';
 import useFilterData from '@/hooks/useFilterData';
 import { ISwiperContentProps } from '@/pages/Home/Home';
+import { memo } from 'react';
 
 const SLIDES_PER_VIEW_AUTO = 'auto';
 const FILTER_MAIN = 'main';
@@ -25,9 +29,31 @@ const SwiperContent = ({ title, filterType }: ISwiperContentProps) => {
     <div className={styles.SwiperContent}>
       <h2 className={styles.title}>{title}</h2>
       <Swiper
+        modules={[Navigation, Pagination]}
+        navigation={{
+          prevEl: '.swiper-button-prev',
+          nextEl: '.swiper-button-next',
+        }}
+        pagination={{
+          el: '.my-swiper-pagination',
+          clickable: true,
+          renderBullet: (_index, className) => {
+            return `<span class="${className} ${styles.bullet}"></span>`;
+          },
+        }}
         className={styles.swiperContainer}
         slidesPerView={SLIDES_PER_VIEW_AUTO}
+        slidesPerGroup={3}
       >
+        <div
+          className={
+            filterType === 'sports' || filterType === 'event'
+              ? styles.hidePagination
+              : ''
+          }
+        >
+          <div className={`my-swiper-pagination ${styles.pagination}`} />
+        </div>
         {filterData.map((item: IImageData, index: number) => (
           <SwiperSlide className={classNames} key={item.id}>
             <img src={item.main?.[filterType]} alt={item.name} />
@@ -39,9 +65,19 @@ const SwiperContent = ({ title, filterType }: ISwiperContentProps) => {
             </span>
           </SwiperSlide>
         ))}
+        <div
+          className={
+            filterType === 'sports' || filterType === 'event'
+              ? styles.hideButton
+              : styles.sliderButton
+          }
+        >
+          <div className={`swiper-button-prev ${styles.prevButton}`} />
+          <div className={`swiper-button-next ${styles.nextButton}`} />
+        </div>
       </Swiper>
     </div>
   );
 };
 
-export default SwiperContent;
+export default memo(SwiperContent);
