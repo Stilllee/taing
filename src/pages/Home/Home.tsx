1f54ee4ca70954c18bf7d683ea488f0030ea8a26
Stilllee <orodae@gmail.com>
2023-09-18 02:34:30 +0900

    ✨ : "메인 배너 UI 완성"

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import styles from './Home.module.scss';
 import Modal from '@components/common/Modal/Modal';
 import PopupModal from '@components/PopupModal/PopupModal';
 import SwiperContent from '@components/common/SwiperContent/SwiperContent.tsx';
+import Button from '@/components/common/Button/Button';
 
 enum FilterTypes {
   MUST = 'must',
@@ -32,10 +33,49 @@ const contentConfig: ISwiperContentProps[] = [
 const Home = () => {
   // 팝업이 처음에 보이도록 상태값을 설정
   const [popupVisible, setPopupVisible] = useState<boolean>(true);
+  const [clickedIndex, setClickedIndex] = useState<null | number>(null);
+  const [isPlayBoxClicked, setIsPlayBoxClicked] = useState<boolean>(false);
+
   const closePopup = () => setPopupVisible(false);
+  const handleClickCircle = (index: number) => {
+    setClickedIndex(index);
+  };
+  const togglePlayBox = () => {
+    setIsPlayBoxClicked(!isPlayBoxClicked);
+  };
 
   return (
     <div className={styles.Home}>
+      <div className={styles.mainBanner}>
+        <img src="/src/assets/images/main_banner.png" alt="재벌집 막내아들" />
+        <p>인생 2회차를 사는 판타지 드라마</p>
+        <Button type={'button'} state={'default'} title={'자세히보기'} />
+        <div className={styles.pageBox}>
+          <div onClick={togglePlayBox} className={styles.playBox}>
+            {isPlayBoxClicked ? (
+              <div className={styles.triangle}></div>
+            ) : (
+              <>
+                <div className={styles.bar}></div>
+                <div className={styles.bar}></div>
+              </>
+            )}
+          </div>
+          <div className={styles.circleBox}>
+            {[0, 1, 2, 3].map((_, index) => (
+              <div
+                key={index}
+                onClick={() => handleClickCircle(index)}
+                className={`${styles.circle} ${
+                  clickedIndex === index ? styles.clickedCircle : ''
+                }`}
+              />
+            ))}
+          </div>
+        </div>
+        <div className={styles.prevBtn} />
+        <div className={styles.nextBtn} />
+      </div>
       {contentConfig.map((config, index) => (
         <SwiperContent
           key={index}
