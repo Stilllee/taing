238670189d41f66895c167a13a1f075c3f927725
chaewon <juh7814@gmail.com>
2023-09-18 11:43:13 +0900

    💄 : "overlay 추가" #39

--- src/components/SearchModal/SearchModal.tsx
+++ src/components/SearchModal/SearchModal.tsx
@@ -1,5 +1,6 @@
 import { currentTimes } from '@/utils/currentTimes';
 import styles from './SearchModal.module.scss';
+
 const SearchModal = () => {
   const popularSearches = [
     '재벌집 막내아들',
@@ -13,33 +14,37 @@ const SearchModal = () => {
     '미씽:그들이 있었다 - 그들을 다만나다',
     '술꾼도시여자들',
   ];
+
   return (
-    <div className={styles.searchModal}>
-      <form className={styles.search}>
-        <input placeholder="TV프로그램, 영화 제목 및 출연진으로 검색해보세요" />
-        <button className={styles.searchIcon} />
-      </form>
-      <div className={styles.searchList}>
-        <div className={styles.recentSearches}>
-          <h2>최근 검색어</h2>
-          <ul>
-            <li>검색 내역이 없습니다.</li>
-          </ul>
-        </div>
-        <div className={styles.popularList}>
-          <h2>실시간 인기 검색어</h2>
-          <ul>
-            {popularSearches.map((search, index) => (
-              <li key={index}>
-                <span>{index + 1}</span>
-                {search}
-              </li>
-            ))}
-          </ul>
-          <div className={styles.currentTime}>{`${currentTimes()} 기준`}</div>
+    <>
+      <div className={styles.searchModal}>
+        <form className={styles.search}>
+          <input placeholder="TV프로그램, 영화 제목 및 출연진으로 검색해보세요" />
+          <button className={styles.searchIcon} />
+        </form>
+        <div className={styles.searchList}>
+          <div className={styles.recentSearches}>
+            <h2>최근 검색어</h2>
+            <ul>
+              <li>검색 내역이 없습니다.</li>
+            </ul>
+          </div>
+          <div className={styles.popularList}>
+            <h2>실시간 인기 검색어</h2>
+            <ul>
+              {popularSearches.map((search, index) => (
+                <li key={index}>
+                  <span>{index + 1}</span>
+                  {search}
+                </li>
+              ))}
+            </ul>
+            <div className={styles.currentTime}>{`${currentTimes()} 기준`}</div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className={styles.overlay} />
+    </>
   );
 };
 
