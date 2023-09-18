790de2142513841cda295b56c4c4a382b97e3715
chaewon <juh7814@gmail.com>
2023-09-18 11:43:34 +0900

    ⚡️ : "스크롤 방지 기능 추가" #39

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,9 @@ import { useScroll, motion, useTransform } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import ProfileModal from '@components/ProfileModal/ProfileModal';
 import SearchModal from '../SearchModal/SearchModal';
+import { useBodyScrollLock } from '@/hooks/useBodyScrollLock';
 const Header = () => {
+  const { lockScroll, openScroll } = useBodyScrollLock();
   const { scrollY } = useScroll();
   const { pathname } = useLocation();
   const backgroundColor = useTransform(
@@ -15,9 +17,14 @@ const Header = () => {
   );
   const [openSearchModal, setOpenSerchModal] = useState(false);
   const [openProfile, setOpenProfile] = useState(false);
-  const onOpenSearchModal = () => setOpenSerchModal(true);
-  const onCloseSearchModal = () => setOpenSerchModal(false);
-
+  const onOpenSearchModal = () => {
+    lockScroll();
+    setOpenSerchModal(true);
+  };
+  const onCloseSearchModal = () => {
+    openScroll();
+    setOpenSerchModal(false);
+  };
   const toggleProfile = () => {
     setOpenProfile(prevOpenProfile => !prevOpenProfile);
   };
