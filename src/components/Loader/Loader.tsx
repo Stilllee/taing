d29499e390e07f7395f227f03b7189e14f23787c
chaewon <juh7814@gmail.com>
2023-09-20 12:23:33 +0900

    💄 : "Loader 컴포넌트 생성" #57

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,17 @@
+import { RotatingLines } from 'react-loader-spinner';
+import styles from './Loader.module.scss';
+const Loader = () => {
+  return (
+    <div className={styles.loader}>
+      <RotatingLines
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible={true}
+      />
+    </div>
+  );
+};
+
+export default Loader;
