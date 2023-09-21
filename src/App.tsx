5bedee58f9ebb11b1051924cec69a746ab1414b7
chaewon <juh7814@gmail.com>
2023-09-21 12:30:32 +0900

    🐛 : "페이지 이동시 스크롤 최상단 고정" #64

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import styles from './App.module.scss';
 import Header from '@components/Header/Header';
 import Footer from '@components/Footer/Footer';
 import { Outlet } from 'react-router-dom';
+import ScrollToTop from '@components/ScrollToTop/ScrollToTop';
 
 function App() {
   return (
     <>
       <Header />
+      <ScrollToTop />
       <div className={styles.mainContent}>
         <Outlet />
       </div>
