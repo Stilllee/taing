2a9fbe1e615bae58eafd385c7f1c103016857f35
Stilllee <orodae@gmail.com>
2023-09-22 22:26:57 +0900

    ✨ : "회원가입 요청시 유저정보 업로드 기능 추가"

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,20 +1,42 @@
+import { useState } from 'react';
+import { useSignUp } from '@/hooks/auth/useSignUp';
+import { useCreateAuthUser } from '@/hooks/firestore/useCreateAuthUser';
 import Input from '@components/common/Input/Input';
-import styles from './SignUp.module.scss';
 import Checkbox from '@components/common/Checkbox/Checkbox';
 import Button from '@components/common/Button/Button';
-import { useState } from 'react';
+import styles from './SignUp.module.scss';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+
+  const { signUp } = useSignUp(true);
+  const { createAuthUser } = useCreateAuthUser();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+    const userCredential = await signUp(email, password);
+    if (userCredential && userCredential.user && userCredential.user.email) {
+      const userAuth = {
+        uid: userCredential.user.uid,
+        email: userCredential.user.email,
+      };
+      await createAuthUser(userAuth);
+    }
+  };
+
   return (
     <main className={styles.SignUp}>
       <div className={styles.titleBox}>
         <h1 className={styles.title}>타잉 회원가입</h1>
         <h4 className={styles.info}>이메일로 간편하게 티빙을 시작하세요!</h4>
       </div>
-      <form className={styles.formBox}>
+      <form className={styles.formBox} onSubmit={handleSubmit}>
         <div className={styles.inputWrapper}>
           <Input
             type={'email'}
