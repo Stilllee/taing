5d5dcf35945b25985be020baa790bd9b2a5f94ea
chaewon <juh7814@gmail.com>
2023-09-25 11:22:27 +0900

    ⚡️ : "로그인 성공여부에 따른 에러메시지와 리다이렉트 기능 추가" #70

--- src/pages/LogIn/LogIn.tsx
+++ src/pages/LogIn/LogIn.tsx
@@ -2,26 +2,51 @@ import Checkbox from '@components/common/Checkbox/Checkbox';
 import styles from './LogIn.module.scss';
 import Button from '@components/common/Button/Button';
 import Input from '@components/common/Input/Input';
-import { Link } from 'react-router-dom';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { useSignIn } from '@/hooks/auth/useSignIn';
 
+const ERROR_MESSAGES = {
+  LOGIN_FAILED: '일치하는 회원정보가 없습니다',
+  WRONG_PASSWORD: '비밀번호가 일치하지 않습니다',
+};
+
+const BUTTON_TITLES = {
+  LOGIN: '로그인하기',
+  LOADING: '로그인 중...',
+};
 const LogIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { isLoading, error, user, signIn } = useSignIn();
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
 
-  const onEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.currentTarget.value);
-  };
-  const onPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.currentTarget.value);
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { type, value } = e.target;
+    if (type === 'email') {
+      setEmail(value);
+    } else if (type === 'password') {
+      setPassword(value);
+    }
   };
 
   const onLogIn = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     signIn(email, password);
   };
+  useEffect(() => {
+    if (error?.message.includes('wrong-password')) {
+      setErrorMessage(ERROR_MESSAGES.WRONG_PASSWORD);
+    } else if (error?.message.includes('user-not-found')) {
+      setErrorMessage(ERROR_MESSAGES.LOGIN_FAILED);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (user) navigate('/');
+  }, [user]);
+
   return (
     <main className={styles.LogIn}>
       <h1 className={styles.title}>TAING 로그인</h1>
@@ -30,18 +55,19 @@ const LogIn = () => {
           type={'email'}
           placeholderText={'이메일'}
           value={email}
-          onChange={onEmailChange}
+          onChange={handleInputChange}
         />
         <Input
           type={'password'}
           placeholderText={'비밀번호'}
           value={password}
-          onChange={onPasswordChange}
+          onChange={handleInputChange}
+          errorMessage={errorMessage && errorMessage}
         />
         <Checkbox id={'auto'} label={'자동로그인'} />
         <Button
           type={'submit'}
-          title={`${isLoading ? '로그인 중...' : '로그인하기'}`}
+          title={isLoading ? BUTTON_TITLES.LOADING : BUTTON_TITLES.LOGIN}
           state={'login'}
         />
       </form>
