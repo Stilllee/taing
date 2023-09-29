1432aa54bb37055d275cc744e4004e43df8fd6a7
Stilllee <orodae@gmail.com>
2023-09-29 16:07:09 +0900

    ✨ : "에러페이지 완성" #94
    - 페이지 UI 완성
    - 홈으로가기 기능 연결

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -2,8 +2,10 @@ import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 import styles from './ErrorPage.module.scss';
 import IconSVG from '@/assets/images/attention_icon.svg';
+import { useCustomNavigate } from '@/hooks/useCustomNavigate';
 
 const ErrorPage = () => {
+  const { navigateTo } = useCustomNavigate();
   return (
     <>
       <Header />
@@ -12,11 +14,16 @@ const ErrorPage = () => {
           <img src={IconSVG} alt="알림 아이콘" />
           <div className={styles.textWrapper}>
             <h1>이런! 현재 해당 페이지를 찾을 수 없습니다.</h1>
-            <p>하지만 티빙에는 더 많은 타잉만의 오리지날 콘텐츠와</p>
+            <p>하지만 타잉에는 더 많은 타잉만의 오리지날 콘텐츠와</p>
             <p>실시간채널, TV프로그램, 영화 콘텐츠가 준비되어 있습니다.</p>
             <p>타잉에서 더 많은 콘텐츠를 즐겨보세요!</p>
           </div>
-          <button className={styles.homeButton}>타잉 홈으로 가기</button>
+          <button
+            onClick={() => navigateTo('/', true)}
+            className={styles.homeButton}
+          >
+            타잉 홈으로 가기
+          </button>
         </section>
       </main>
       <Footer />
